6c9d7e325f5ea4325fc3b98100ebbecb8a421fa5
Yue Chen <dspfac@gmail.com>
2015-06-30 19:53:13 +0000

    commit exercise content by Live4Code

--- greaterNum.js
+++ greaterNum.js
@@ -1,5 +1,5 @@
 function greaterNum(num1, num2) {
-    if ( ??? ) {            // FILL IN YOUR ANSWER HERE
+    if ( num1 >= num2 ) {            // FILL IN YOUR ANSWER HERE
         return num1;
     } else {
         return num2;
