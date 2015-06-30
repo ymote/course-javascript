f9e3e74078e0c1e5082ede00d357924448dbb743
Yue Chen <dspfac@gmail.com>
2015-06-30 19:59:22 +0000

    commit exercise content by Live4Code

--- daysOfWeek.js
+++ daysOfWeek.js
@@ -1,6 +1,6 @@
 function daysOfWeek () {
 
-    var days = [ 'Sunday', 'Monday' ];   // FILL IN YOUR ANSWER HERE
+    var days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];   // FILL IN YOUR ANSWER HERE
 
     for (var i = 0; i < days.length; i++) {
         console.log('Day #' + (i + 1) + ' is ' + days[i]);
