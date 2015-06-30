1dff4ba0b35256bb6cb0d6423abfdb3754615a9b
Yue Chen <dspfac@gmail.com>
2015-06-30 19:51:59 +0000

    commit exercise content by Live4Code

--- calcArea.js
+++ calcArea.js
@@ -3,7 +3,7 @@
   var circumference = Math.PI * 2 * radius;
   console.log("The circumference is " + circumference);
 
-  var area = ???;  // FILL IN THE FORMULA HERE
+  var area = Math.PI*radius*radius;  // FILL IN THE FORMULA HERE
   console.log("The area is " + area);
 
   return area;
